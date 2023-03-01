6720a0950821f217b136a6dcab1886d8f517e5a7
Alex Mata Barrero <alex.mata@estudiantat.upc.edu>
2023-03-01 10:35:55 +0100

    Fins exercici 2

--- t1.py
+++ t1.py
@@ -1,28 +1,120 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sf
+import sounddevice as sd
+from numpy.fft import fft
 
-# creem i guardem senyal sinusoidal amb següents parametres:
+### Exercici 1 ###
 
-T= 2.5                               # Durada de T segons
-fm=8000                              # Freqüència de mostratge en Hz
-fx=4000                              # Freqüència de la sinusoide
-A=4                                  # Amplitud de la sinusoide
-pi=np.pi                             # Valor del número pi
-L = int(fm * T)                      # Nombre de mostres del senyal digital
-Tm=1/fm                              # Període de mostratge
-t=Tm*np.arange(L)                    # Vector amb els valors de la variable temporal, de 0 a T
-x = A * np.cos(2 * pi * fx * t)      # Senyal sinusoidal
-sf.write('so.wav', x, fm)   # Escriptura del senyal a un fitxer en format wav
+## Senyal 4kHz
+T= 2.5                               
+fm=8000                              
+fx=4000                              
+A=4                                  
+pi=np.pi                            
+L = int(fm * T)                      
+Tm=1/fm                              
+t=Tm*np.arange(L)                    
+x = A * np.cos(2 * pi * fx * t)      
+sf.write('ex1_4khz.wav', x, fm)   
+Tx=1/fx                                   
+Ls=int(fm*5*Tx)    
 
-# reresentem gràficament 5 períodes del senyal:
+# Representació
+plt.figure(0)                            
+plt.plot(t[0:Ls], x[0:Ls])                
+plt.xlabel('t en segons')                 
+plt.title('5 periodes de la sinusoide 4kHz')
+plt.savefig('5_periodes_4khz.png')
+plt.show()                                
+sd.play(x, fm)    
 
-x_r, fm = sf.read('so.wav')
-Tx=1/fx                                   # Període del senyal
-Ls=int(fm*5*Tx)                           # Nombre de mostres corresponents a 5 períodes de la sinusoide
+# Domini transformat
+N=5000                       
+X=fft(x[0 : Ls], N)           
+k=np.arange(N)                       
+plt.figure(1)                         
+plt.subplot(211)                      
+plt.plot(k,abs(X))                    
+plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   
+plt.ylabel('|X[k]|')                  
+plt.subplot(212)                      
+plt.plot(k,np.unwrap(np.angle(X)))    
+plt.xlabel('Index k')                 
+plt.ylabel('$\phi_x[k]$')   
+plt.savefig('TF_4khz.png')
+plt.show()
 
-plt.figure(0)                             # Nova figura
-plt.plot(t[0:Ls], x[0:Ls])                # Representació del senyal en funció del temps
-plt.xlabel('t en segons')                 # Etiqueta eix temporal
-plt.title('5 periodes de la sinusoide')   # Títol del gràfic
-plt.show()                                # Visualització de l'objecte gràfic.
+## Senyal 1kHz
+T= 2.5                              
+fm=8000                            
+fx=1000                             
+A=4                               
+pi=np.pi                            
+L = int(fm * T)                      
+Tm=1/fm                              
+t=Tm*np.arange(L)                    
+x = A * np.cos(2 * pi * fx * t)     
+sf.write('ex1_1khz.wav', x, fm)   
+Tx=1/fx                                  
+Ls=int(fm*5*Tx)                          
+
+# Representació
+plt.figure(2)                             
+plt.plot(t[0:Ls], x[0:Ls])                
+plt.xlabel('t en segons')                
+plt.title('5 periodes de la sinusoide 1kHz')
+plt.savefig('5_periodes_1khz.png')
+plt.show()                              
+sd.play(x, fm)    
+
+# Domini transformat
+N=5000                        
+X=fft(x[0 : Ls], N)           
+k=np.arange(N)                      
+plt.figure(3)                        
+plt.subplot(211)                      
+plt.plot(k,abs(X))                   
+plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   
+plt.ylabel('|X[k]|')               
+plt.subplot(212)                     
+plt.plot(k,np.unwrap(np.angle(X)))  
+plt.xlabel('Index k')                 
+plt.ylabel('$\phi_x[k]$')
+plt.savefig('TF_1khz.png')
+plt.show() 
+
+
+### Exercici 2 ###
+x_r, fm = sf.read('ex1_4khz.wav')
+Tm=1/fm
+t=Tm*np.arange(len(x_r))
+fx=fm/2
+Tx=1/fx
+Ls=int(fm*5*Tx)
+plt.figure(4)
+plt.plot(t[0:Ls], x_r[0:Ls])
+plt.xlabel('t en segons')      
+plt.title('5 periodes de la sinusoide 4kHz ex2')
+plt.savefig('ex2_4khz.png')
+plt.show()
+
+sd.play(x_r, fm)    
+
+N=5000                       
+X=fft(x_r[0 : Ls], N)         
+k=np.arange(N)                        
+plt.figure(5)                        
+plt.subplot(211)                     
+plt.plot(k,abs(X))                   
+plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   
+plt.ylabel('|X[k]|')                 
+plt.subplot(212)                      
+plt.plot(k,np.unwrap(np.angle(X)))   
+plt.xlabel('Index k')               
+plt.ylabel('$\phi_x[k]$')
+plt.savefig('ex2_TF_4khz.png')
+plt.show()    
+
+
+### Exercici 3 ###
